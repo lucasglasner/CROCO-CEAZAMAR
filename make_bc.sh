deac319f50c30583080defb55bbfa567aa0f92d5
lucas.glasner <lucas.glasner@ceaza.cl>
2023-05-12 18:43:23 -0300

    now before running the preprocessing script, the program makes sure the global files exists

--- make_bc.sh
+++ make_bc.sh
@@ -8,11 +8,29 @@
 #
 maindir='/ceaza/lucas/CROCO-CEAZAMAR/'
 cd $maindir
-
-SECONDS=0
 echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
 echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CREATING CROCO BOUNDARY CONDITIONS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
 echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+
+echo 'Checking if global forecasts have arrived...'
+ohindcast=$maindir/DATA/MERCATOR/HINDCAST/$(date -d '6 day ago' +%F -u).nc
+ahindcast=$maindir/DATA/ERA5/$(date -d '6 day ago' +%F -u).nc
+oforecast=$maindir/DATA/MERCATOR/$(date +%F -u).nc
+if [ ! -f $ohindcast ]; then
+    echo "Ocean hindcast file $ohindcast doesnt exists!"
+    exit
+fi
+if [ ! -f $ahindcast ]; then
+    echo "Atmospheric hindcast file $ahindcast doesnt exists!"
+    exit
+fi
+if [ ! -f $oforecast ]; then
+    echo "Ocean forecast file $oforecast doesnt exists!"
+    exit
+fi
+echo 'All good running "make*" scripts...'
+echo '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+SECONDS=0
 now=$(date +%F\ %H:%M:%S -u)
 echo $now Creating hindcast atmospheric boundary conditions...
 ./make_blk_hindcast.py
